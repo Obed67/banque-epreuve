e9fb9739137b227a2afeda43e5a69fedfd8cd51b
Obed67 <obedagbohouns@gmail.com>
2026-04-11 00:06:28 +0100

    fix: delete metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,24 +4,6 @@ import { Inter } from "next/font/google";
 import AppShell from "./components/AppShell";
 
 const inter = Inter({ subsets: ["latin"] });
-const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "https://banque-epreuve.vercel.app";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(siteUrl),
-  title: "Portail d'Épreuve",
-  description:
-    "Plateforme de gestion et de partage d'épreuves et de ressources académiques",
-  openGraph: {
-    title: "Portail d'Épreuve",
-    description:
-      "Plateforme de gestion et de partage d'épreuves et de ressources académiques",
-    url: siteUrl,
-    siteName: "Portail d'Épreuve",
-    locale: "fr_FR",
-    type: "website",
-  },
-};
 
 export default function RootLayout({
   children,
