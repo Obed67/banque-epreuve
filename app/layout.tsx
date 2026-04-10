8079224fd097af3aa82dd2951fa739928021b1e9
Obed67 <obedagbohouns@gmail.com>
2026-04-11 00:15:21 +0100

    fix: add icon to favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import AppShell from "./components/AppShell";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Banque Epreuve",
+  description:
+    "Plateforme web de partage de documents académiques, d'epreuves et de ressources.",
+  icons: {
+    icon: "/icon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
